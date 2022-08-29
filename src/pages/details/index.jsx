04a9fa9b0fb10412380a939ce7ef20697950ec9c
Kaique Martins <kaiquemartinsdasilvaferreira@gmail.com>
2022-08-29 18:12:14 -0300

    update css in js

--- src/pages/details/index.jsx
+++ src/pages/details/index.jsx
@@ -1,9 +1,9 @@
-import './details.css';
+import { fontConfig } from './style';
 
 export function Details(){
   return(
-    <>
+    <fontConfig>
     <h1>Hello World!👋</h1>
-    </>
+    </fontConfig>
 )
 }
---
+++ src/pages/details/style.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const fontConfig = styled.h1`
+  color: blue;
+`
