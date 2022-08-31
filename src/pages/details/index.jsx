c15e9a4ca46d7b67c7a849ae5ded81173ac10a1d
Kaique Martins <kaiquemartinsdasilvaferreira@gmail.com>
2022-08-31 16:39:43 -0300

    insert sections

--- src/pages/details/index.jsx
+++ src/pages/details/index.jsx
@@ -1,12 +1,23 @@
-import { Container } from './style';
+import { Container, Links } from './style';
 import { Button } from '../../components/Button'
 import { Header } from '../../components/Header';
+import { Section } from '../../components/Section';
 
 export function Details() {
 
   return (
     <Container>
     <Header/>
+    <Section title="Links">
+    <Links>
+      <li><a href="@">https://rocketseat.com.br</a></li>
+      <li><a href="@">http://rocketseat.com.br</a></li>
+    </Links>
+    {/* Title passed in a  property but children capture everything is inside of Section*/}
+    </Section>
+    <Section title="Markers">
+    
+    </Section>
     <Button title="Return" ></Button>
     </Container>
 )
