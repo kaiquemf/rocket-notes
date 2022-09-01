083f6b6516c8121c0654d903ac0c174ae3244d7c
Kaique Martins <kaiquemartinsdasilvaferreira@gmail.com>
2022-08-31 21:44:35 -0300

    create SignIn page

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,26 @@
+import { FiMail, FiLock} from 'react-icons/fi'
+
+import { Container, Form, Background } from './style'
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+import { ButtonText } from '../../components/ButtonText'
+ 
+export function SignIn(){
+  return(
+  <Container>
+   <Form>
+    <h1>Rocket Notes</h1>
+    <p>Application to save and manage your useful links.</p>
+
+    <Input placeholder="E-mail" type="text" icon={FiMail} />
+    <Input placeholder="Password" type="password" icon={FiLock} />
+
+    <Button title="login"/>
+    <a href="#">
+    <ButtonText title="Create Account" isActive></ButtonText>
+    </a>
+   </Form>
+    <Background />
+  </Container>
+  );
+}
---
+++ src/pages/SignIn/style.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+import backgroundIMG from '../../assets/NotesImage.jpg'
+
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  align-items: stretch;
+`
+export const Form = styled.form`
+  padding: 0 13.6rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  > h1{
+    font-size: 4.8rem;
+    line-height: 6.3rem;
+    font-weight: 700;
+
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+  }
+  
+  > p{
+    font-size: 1.4rem;
+    line-height: 1.8rem;
+    color: ${({ theme }) => theme.COLORS.GRAY_300};
+    
+    margin-bottom: 3rem;
+  }
+
+  > a{
+    margin-top: 6rem;
+  }
+`
+export const Background = styled.div`
+  flex: 1;
+  background: url(${backgroundIMG}) no-repeat center center;
+  background-size: cover;
+
+  opacity: 0.65;
+
+  border-left: 1px solid ${({ theme }) => theme.COLORS.BACKGROUND_700};
+`
