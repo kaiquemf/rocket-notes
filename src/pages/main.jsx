9fc9840af759920b1b5d0ca1679a9e116b7397ca
Kaique Martins <kaiquemartinsdasilvaferreira@gmail.com>
2022-09-01 19:57:41 -0300

    customize routes in main page

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -2,14 +2,14 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../styles/global';
 import ReactDOM from 'react-dom/client';
 import theme from '../styles/theme';
-import { NewNote } from './NewNote';
+import { Routes } from '../routes';
 import React from 'react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
     <GlobalStyles />
-    <NewNote/>
+    <Routes/>
     </ThemeProvider>
   </React.StrictMode>
 )
